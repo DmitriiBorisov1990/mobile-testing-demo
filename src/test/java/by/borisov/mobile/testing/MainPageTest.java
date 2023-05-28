a3ebdc1c2c3b319f803fa729ea02f4107a85a1f9
Dmitrii Borisov <dmitrii.borisov1990@gmail.com>
2023-05-28 19:18:04 +0300

    create git repo

--- src/test/java/by/borisov/mobile/testing/MainPageTest.java
+++ src/test/java/by/borisov/mobile/testing/MainPageTest.java
@@ -1,21 +1,20 @@
 package by.borisov.mobile.testing;
 
-import by.borisov.mobile.testing.pages.BasePage;
 import by.borisov.mobile.testing.pages.MainPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class MainPageTest extends BasePage {
+public class MainPageTest extends BaseTest {
 
     @Test
     public void clickOnMainButton() {
         MainPage mainPage = new MainPage();
         try {
-            Thread.sleep(50L);
+            Thread.sleep(5000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         mainPage.clickOnMainPageButton();
-        Assert.assertEquals(2,3);
+        //Assert.assertEquals(2,3);
     }
 }
