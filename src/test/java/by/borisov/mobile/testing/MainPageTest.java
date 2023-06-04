d2da658c6d8c0cfa61a6acd4bfb83ce9159c451a
Dmitrii Borisov <dmitrii.borisov1990@gmail.com>
2023-06-04 13:37:16 +0300

    add waiters

--- src/test/java/by/borisov/mobile/testing/MainPageTest.java
+++ src/test/java/by/borisov/mobile/testing/MainPageTest.java
@@ -1,6 +1,10 @@
 package by.borisov.mobile.testing;
 
+import by.borisov.mobile.testing.driver.DriverManager;
 import by.borisov.mobile.testing.pages.MainPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,11 +13,9 @@ public class MainPageTest extends BaseTest {
     @Test
     public void clickOnMainButton() {
         MainPage mainPage = new MainPage();
-        try {
-            Thread.sleep(5000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        By id = By.id("ru.ozon.app.android:id/menu_main");
+        WebDriverWait webDriverWait = new WebDriverWait(DriverManager.getDriver(), 5);
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(id));
         mainPage.clickOnMainPageButton();
         //Assert.assertEquals(2,3);
     }
